bbf66e4ab970e7ba0c5c88f84792edc7b5a73baa
Matt Swieboda <mswiebod@gmail.com>
2020-07-12 21:41:48 -0500

    adds lobby create, start, stop, end endpoints

--- app.rb
+++ app.rb
@@ -5,23 +5,65 @@ require 'json'
 
 class App < Roda
   plugin :all_verbs
+  plugin :json_parser
+  plugin :error_handler
+
+  error do |e|
+    message = e.message
+
+    if e.is_a?(ActiveRecord::RecordNotFound)
+      response.status = 404
+      puts e
+      puts e.backtrace
+      message = "Not found"
+    end
+
+    message.to_json
+  end
 
   route do |r|
     r.on "lobbies" do
-      # GET /lobbies/5
+      # POST /lobbies/create
+      r.post "create" do
+        Lobby.create(
+          name: r.params['name'],
+          host: r.params['host'],
+          peers: 1,
+          status: "open",
+          size: r.params['size']
+        ).to_json
+      end
+
       r.on Integer do |lobby_id|
         lobby = Lobby.find(lobby_id)
 
-        # POST /lobbies/5/join
+        # POST /lobbies/:id/join
         r.post "join" do
           lobby.update!(peers: lobby.peers + 1)
           lobby.to_json
         end
 
-        # DELETE /lobbies/5/leave
+        # POST /lobbies/:id/start
+        r.post "start" do
+          lobby.update!(status: "active")
+          lobby.to_json
+        end
+
+        # POST /lobbies/:id/stop
+        r.post "stop" do
+          lobby.update!(status: "open")
+          lobby.to_json
+        end
+
+        # DELETE /lobbies/:id/leave
         r.delete "leave" do
           lobby.update!(peers: lobby.peers - 1)
           lobby.to_json
+        end
+
+        # DELETE /lobbies/:id/end
+        r.delete "end" do
+          lobby.destroy.to_json
         end
 
         r.is do
