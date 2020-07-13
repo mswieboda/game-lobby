fba94cd8717ee4d50174958275e05e2ba8c58093
Matt Swieboda <mswiebod@gmail.com>
2020-07-12 19:21:45 -0500

    moves app out of config.ru

---
+++ app.rb
@@ -0,0 +1,30 @@
+require "./config/environment"
+require "json"
+
+class App < Roda
+  route do |r|
+    r.on "lobbies" do
+      # GET /lobbies
+      r.get do
+        [{ip: "123.168.1.1", name: "foo", status: "open", peers: 5, max_peers: 10}].to_json
+      end
+
+      # GET /lobbies/5
+      r.on Integer do |lobby_id|
+        r.get do
+          {ip: "123.168.1.1", name: "foo", status: "open", peers: 5, max_peers: 10}.to_json
+        end
+
+        # POST /lobbies/5/join
+        r.post "join" do
+          {ip: "123.168.1.1", name: "foo", status: "open", peers: 6, max_peers: 10}.to_json
+        end
+
+        # POST /lobbies/5/leave
+        r.post "leave" do
+          {ip: "123.168.1.1", name: "foo", status: "open", peers: 5, max_peers: 10}.to_json
+        end
+      end
+    end
+  end
+end
